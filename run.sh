23c9af05f0597879ea59840df08f829cf93d9218
rquinzio <remi.quinzio@outlook.fr>
2021-07-04 19:20:05 +0200

    typo

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 
 docker network create nextcloud_network --subnet 172.28.0.0/16 --ip-range 172.28.0.0/24
 docker network create le_network --subnet 172.29.0.0/16 --ip-range 172.29.0.0/24
